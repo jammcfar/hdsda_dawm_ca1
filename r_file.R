2e7bda20d9d50353fde26e7a10ae29af691ae288
jam <jam@pop-os.localdomain>
2020-11-23 22:09:52 +0000

    Working on cv

--- r_file.R
+++ r_file.R
@@ -1,254 +1,162 @@
-### Title:  R analysis file
+### Title:  Association rules analysis file
 ### Author: x19175329@student.ncirl.ie
-### Desc:   All-in-one analysis file
+### Desc:   All-in-one analysis file for DAWM CA1
 
-### General
+# Set seed
 set.seed(11111987)
+
+# Packages
 library(tidyverse)
-library(tidymodels)
 library(here)
-library(naniar)
 library(arules)
 library(arulesViz)
 library(shinythemes)
 
-### Part 1: Association rules
-
-##Preview data
+# Get data length
 g_lines <- readLines("Groceries.csv")
 
-##fix dates not being seperated
-##date_rgx <- "\\d+/\\d+/\\d+"
-
-## this way is good, but comment out for now
-#g_dates <- str_extract(g_lines, date_rgx)
-#g_nodates <- str_remove(g_lines, date_rgx)
-##
-#### convert to list
-#### remove last two chars from every row in vector
-#g_nodates_s <- substr(g_nodates, 1, nchar(g_nodates) - 2)
-#g_nodates_l <- str_split(g_nodates_s, ", ")
-#names(g_nodates_l) <- g_dates
-##
-#g_trans <- as(g_nodates_l, "transactions")
-##
-#inspect(head(g_trans))
-#
-#itemFrequencyPlot(g_trans)
-#
-#image(g_trans[1:10])
-#
-#arls <- apriori(g_trans)
-#
-#plot(arls)
-#inspectDT(arls)
-#ruleExplorer(arls)
-
-##that was fine, but really not sure if thats what Enda wants...
-#this bit here is not used
-#g_in <- read_csv("Groceries.csv",
-                 #col_names = F,
-                 #trim_ws = T,
-                 #)
-
-##actually do this here
-#all the row-lengths
+# Get the max row width so we can fill with NAs
 g_row_l <- count.fields("Groceries.csv")
 g_row_l_max <- max(g_row_l)
 
 g_in <-
-read.table("Groceries.csv",
-           sep = ",",
-           header = F,
-           fill = T,
-           col.names = paste("x", seq(1:g_row_l_max)))
+  read.table("Groceries.csv",
+    sep = ",",
+    header = F,
+    fill = T,
+    col.names = paste("x", seq(1:g_row_l_max))
+  )
 
+# A regex to grab the dates from malformed column
 date_rgx <- "\\d+/\\d+/\\d+"
 
 
 g_datefix <-
   g_in %>%
   as_tibble() %>%
-  mutate(dates = str_extract(x.1, date_rgx),
-         x.1 = str_remove(x.1, date_rgx))
+  mutate(
+    dates = str_extract(x.1, date_rgx),
+    x.1 = str_remove(x.1, date_rgx)
+  )
 
-#number of unique days
+# number of unique days
 n_dates <- length(unique(g_datefix$dates))
 
 g_tidy <-
   g_datefix %>%
-    rowid_to_column("id_var") %>%
-    pivot_longer(cols = x.1:x.54, values_to = "items") %>%
-    mutate(items = trimws(items)) %>%
-    select(id_var, items) %>%
-    filter(items != "",
-        items != "") %>%
-    group_by(id_var) %>%
+  rowid_to_column("id_var") %>%
+  pivot_longer(cols = x.1:x.54, values_to = "items") %>%
+  mutate(items = trimws(items)) %>%
+  select(id_var, items) %>%
+  filter(
+    items != "",
+    items != ""
+  ) %>%
+  group_by(id_var) %>%
   distinct() %>%
-    mutate(item_no = 1:n()) %>%
-    ungroup()
+  mutate(item_no = 1:n()) %>%
+  ungroup()
 
-##quick exploration
+## quick exploration
 g_for_sum <-
-    g_tidy %>%
-    group_by(id_var) %>%
-    summarise(n_by_id = n()) %>%
-    summarise(Min = min(n_by_id),
-              Median = median(n_by_id),
-              Max = max(n_by_id),
-              Mode = modeest::mlv(n_by_id)
-              )
+  g_tidy %>%
+  group_by(id_var) %>%
+  summarise(n_by_id = n()) %>%
+  ungroup() %>%
+  summarise(
+    Min = min(n_by_id),
+    Median = median(n_by_id),
+    Max = max(n_by_id),
+    Mode = modeest::mlv(n_by_id)
+  )
 
 ## quick hist (redo)
 hist(g_tidy %>%
-     group_by(id_var) %>%
-     summarise(n_by_id = n()) %>% select(n_by_id) %>% as_vector(),
-     breaks = 25, xlim = c(0,30))
+  group_by(id_var) %>%
+  summarise(n_by_id = n()) %>% select(n_by_id) %>% as_vector(),
+breaks = 25, xlim = c(0, 30)
+)
 
 ## make into list, then coerce to transactions object
 g_list <- split(g_tidy$items, g_tidy$id_var)
 
-g_trans <- as(object = g_list,
-              Class = "transactions")
+g_trans <- as(
+  object = g_list,
+  Class = "transactions"
+)
 
+## Explore transactions
+# Quick summary of transactions
 g_summ <- summary(g_trans)
-g_summ
-## Do item frequencies
+(g_summ)
 
-freq_params <- list(supp = 0.1, minlen = 2, maxlen = 4)
+# Check data structure for obvious issues
+image(g_trans[1:50, ], )
 
-g_freq <- eclat(g_trans, parameter = freq_params)
-
-##inspect(g_freq)
-itemFrequencyPlot(g_trans, topN = 10, type = "absolute", main = "Item Frequency")
-itemFrequencyPlot(g_trans, support = 0.1, topN = 10)
-image(g_trans[1:200,],)
-
-head(inspect(g_freq))
-
-#support rule of thumb
+# use a rule-of-thumb for support (i.e. bought twice a day)
 supp_rot <- (n_dates * 2) / nrow(g_tidy)
 
-ap_params <- list(support = 0.10,
-                  confidence = supp_rot,
-                  minlen = 2,
-                  maxlen = 10,
-                  maxtime = 20,
-                  target = "rules")
+# Do item frequencies
+itemFrequencyPlot(g_trans, topN = 10, type = "absolute", main = "Item Frequency")
+itemFrequencyPlot(g_trans, support = supp_rot, topN = 10)
+
+## Run Apriori
+# Set up parameters
+ap_params <- list(
+  support = supp_rot,
+  confidence = 0.25,
+  minlen = 2,
+  maxlen = 10,
+  maxtime = 20,
+  target = "rules"
+)
 
 fit <- apriori(g_trans,
-               parameter = ap_params)
+  parameter = ap_params
+)
 
 summary(fit)
-
-inspect(fit)
 
 inspect(sort(fit, by = "confidence")[1:20])
 inspect(sort(fit, by = "lift")[1:20])
 
-##most interesting rules lie on the support/confidence border (sc-optimal rules)Bayardo, Jr RJ, Agrawal R (1999)
+## most interesting rules lie on the support/confidence border (sc-optimal rules)Bayardo, Jr RJ, Agrawal R (1999)
 plot(fit)
 plot(fit, method = "two-key plot")
 
+## Plot rules in a grid
+plot(fit, method = "grouped")
 
-top_rules <- head(fit, n = 15, by = "lift")
+top_rules <- head(fit, n = 20, by = "lift")
+inspect(sort(top_rules, by = "lift"))
 plot(top_rules, method = "graph")
 
-arulesViz::ruleExplorer(fit)
+# arulesViz::ruleExplorer(fit)
 inspect(fit)
 
-##get ones that aren't vegetables
+## get ones that aren't vegetables
 `%notin%` <- Negate(`%in%`)
-fit_noveg <-subset(fit, items %notin% "vegetables")
+fit_noveg <- subset(fit, items %notin% "vegetables")
 
 inspect(sort(fit_noveg, by = "lift")[1:20])
-top_noveg_rules <- head(fit_noveg, n = 30, by = "confidence")
+top_noveg_rules <- head(fit_noveg, n = 10, by = "lift")
 plot(top_noveg_rules, method = "graph")
-#pasta and paper towels is a good one that might be actionable
-
-### Part 2
-
-## Data in
-#
-#df_in <- read_csv(here("adult.data"),
-                  #col_names = F)
-#
-#rand_samp = sample(1:nrow(df_in),
-                   #size = 10000,
-                   #replace = F
-                    #)
-#
-#df_samp <- df_in[rand_samp, ]
-#
-## column names come in a seperate information file
-#col_names_in <- read_lines(
-  #here("adult.names")
-#)
-#
-#col_strings_raw <- str_match_all(col_names_in, "\\w+(?=.*:)")
-#
-#col_strings_clean <- c()
-#
-#for(i in 97:length(col_strings_raw)){
-#
-  #if(length(col_strings_raw[[i]]) > 1){
-#
-     #foo_string <- paste(col_strings_raw[[i]][1],
-           #col_strings_raw[[i]][2],
-           #sep = "_"
-           #)
-#
-    #col_strings_clean[i-96] <- foo_string
-    #} else{
-#
-    #col_strings_clean[i-96] <- col_strings_raw[[i]][1]
-      #}
-#}
-#
-#col_strings_clean
-#
-#glimpse(df_samp)
-#
-#colnames(df_samp) <- c(col_strings_clean, "target")
-#
-#df_samp
-#
-###inspect the data
-#common_na_strings
-#df_nas <-
-  #df_samp %>% replace_with_na_all(
-              #condition = ~.x %in% common_na_strings)
-#
-#
-#vis_miss(x = df_nas)
-#
-#View(df_nas)
 
 
-#
-#g_wide_bin <-
-  #g_tidy %>%
-  ###rowid_to_column("id_var") %>%
-#mutate(dummy = 1) %>%
-  #pivot_wider(names_from = item_no,
-              #values_from = items,
-              #id_cols = c(id_var)) %>%
-##mutate_at(vars(yogurt:sugar), ~ ifelse(is.na(.), NA, 1)) %>%
-##mutate_at(vars(yogurt:sugar), ~ as.factor(.)) %>%
-#select(everything())
-#
-##g_list <-
-  ##g_tidy %>%
-  ##select(-item_no) %>%
-  ##group_by(id_var) %>%
-  ##group_split()
-#
-##g_trans <-
-  ##head(as.list(g_wide_bin)) %>%
-  ##as(
-              ##Class = "transactions")
-#
-#as.list(g_wide_bin %>% group_by(id_var))
-#
-#g_trans <- as(object = g_list,
-              #Class = "transactions")
+ap_params_2 <- list(
+  support = supp_rot,
+  confidence = 0.25,
+  minlen = 2,
+  maxlen = 2,
+  maxtime = 20,
+  target = "rules"
+)
+
+fit_2 <- apriori(g_trans,
+  parameter = ap_params_2
+)
+
+summary(fit_2)
+
+inspect(sort(fit_2, by = "confidence")[1:20])
+inspect(sort(fit_2, by = "lift")[1:20])
--- r_file_prt3.R
+++ r_file_prt3.R
@@ -8,79 +8,118 @@ library(tidyverse)
 library(tidymodels)
 library(here)
 library(naniar)
-library(naivebayes)
+## library(naivebayes)
 library(recipeselectors)
 library(themis)
-##add a user choice here, incase he doesnt want to DL off github or dont if it goes in appendix
+library(corrr)
+
+## add a user choice here, incase he doesnt want to DL off github or dont if it goes in appendix
 "https://archive.ics.uci.edu/ml/machine-learning-databases/00222/bank-additional.zip"
 
 df_bank_in <-
-    read_delim(here("bank-additional-full.csv"),
-               delim = ";")
+  read_delim(here("bank-additional-full.csv"),
+    delim = ";"
+  )
 
-rand_samp = sample(1:nrow(df_bank_in),
-                   size = 10000,
-                   replace = F
-                   )
+rand_samp <- sample(1:nrow(df_bank_in),
+  size = 10000,
+  replace = F
+)
 
 df_bank_samp <- df_bank_in[rand_samp, ]
 
 ## set colname to snakecase, as per tidy style guide
 colnames(df_bank_samp) <- snakecase::to_snake_case(colnames(df_bank_samp))
 
-##remove this column, and set pdays to NA, as per dataset documentation
+## remove this column, and set pdays to NA, as per dataset documentation
 ## justification for pdays as missing https://medium.com/@abbdar/first-steps-in-machine-learning-predicting-subscription-for-bank-deposits-866516b90e4
 
 df_bank_proc <-
   df_bank_samp %>%
   mutate(pdays = if_else(pdays != 999,
-                         pdays,
-                         NA_real_)) %>%
+    pdays,
+    NA_real_
+  )) %>%
   select(-duration)
 
-#funModeling::status(df_bank_in)
+# funModeling::status(df_bank_in)
 
-custom_na_strings <- c(common_na_strings, "nonexistent",  "unknown")
+custom_na_strings <- c(common_na_strings, "nonexistent", "unknown")
 
 df_bank_nas <-
   df_bank_proc %>% replace_with_na_all(
-                condition = ~.x %in% custom_na_strings)
+    condition = ~ .x %in% custom_na_strings
+  )
 
-#funModeling::status(df_nas)
+funModeling::status(df_bank_nas)
 
-##convert all characters to factors
+## convert all characters to factors
+## also remove pdays and poutcome, these are problematic
+## and their correct encoding relies on SME knowledge
 df_clean <-
   df_bank_nas %>%
+  select(-pdays, -poutcome) %>%
   mutate_if(is.character, as.factor) %>%
   mutate(y = as_factor(y))
 
+# Checking for intercorrelations (stick in appendix)
+glimpse(df_clean) %>%
+  select(where(is.numeric)) %>%
+  correlate() %>%
+  pivot_longer(age:nr_employed) %>%
+  arrange(desc(value))
+
 ## lets try tidymodels. etc
-df_split <- initial_split(df_clean, prop = 3/4)
+df_split <- initial_split(df_clean, prop = 3 / 4)
 
 # Create data frames for the two sets:
 df_train <- training(df_split)
-df_test  <- testing(df_split)
+df_test <- testing(df_split)
+
 
 bank_rec <-
   recipe(y ~ ., data = df_train) %>%
-  update_role(y, new_role = "outcome") %>%
-  step_novel(all_predictors(), -all_numeric()) %>%
-  step_unknown(all_predictors(), -all_numeric()) %>%
-  step_dummy(all_nominal(), -all_outcomes()) %>%
-  step_zv(all_predictors()) #%>%
-#  step_select_roc(all_predictors(),
-#                  threshold = 0.6,
-#                  outcome = "y") %>%
-#  step_rose(y) #actually made no difference to rpart?
+    update_role(y, new_role = "outcome") %>%
+    # step_novel(all_predictors(), -all_numeric()) %>%
+    step_unknown(all_predictors(), -all_numeric()) %>%
+    step_dummy(all_nominal(), -all_outcomes()) %>%
+    step_zv(all_predictors()) %>%
+
+    #  step_select_roc(all_predictors(),
+    #                  threshold = 0.6,
+    #                  outcome = "y") %>%
+    #  step_rose(y) #actually made no difference to rpart?
+
+    # tree based models can handle these bninaries as numerics
+
+    # Pre-process the data
+    bank_prep() <- prep(bank_rec, training = df_train)
+df_baked_train <- bake(bank_prep, df_train)
+
+
+
+foo_train_recipe <- prep(pre_processor(df_train), training = df_train)
+training_processed <- bake(foo_train_recipe, new_data = df_training)
+
+## cross-validation
+df_train_cvs <-
+  mc_cv(df_baked_train,
+    prop = 0.75,
+    times = 10
+  )
+
+
 
 nb_mod <-
   discrim::naive_Bayes() %>%
   set_engine("naivebayes")
 
 rf_mod <-
-  rand_forest(mtry = 3,
-              trees = 200,
-              min_n = 20) %>%
+  rand_forest(
+    mtry = 3,
+    trees = 200,
+    min_n = 20
+  ) %>%
   set_engine("ranger") %>%
   set_mode("classification")
 
@@ -90,44 +129,45 @@ rpart_mod <-
   set_mode("classification")
 
 
-#bank_nb_workflow <-
-  #workflow() %>%
-  #add_model(nb_mod) %>%
-  #add_recipe(bank_rec)
+# bank_nb_workflow <-
+# workflow() %>%
+# add_model(nb_mod) %>%
+# add_recipe(bank_rec)
 #
-###step_novel
+### step_novel
 #
-#bank_fit <-
-  #bank_nb_workflow %>%
-  #fit(data = df_train)
+# bank_fit <-
+# bank_nb_workflow %>%
+# fit(data = df_train)
 #
-#nb_pred <-
-    #predict(bank_fit,
-            #df_test,
-            #type = "prob") %>%
-  #bind_cols(df_test %>% select(y))
+# nb_pred <-
+# predict(bank_fit,
+# df_test,
+# type = "prob") %>%
+# bind_cols(df_test %>% select(y))
 #
-#nb_pred %>%
-  #roc_curve(truth = y, .pred_no) %>%
-  #autoplot()
+# nb_pred %>%
+# roc_curve(truth = y, .pred_no) %>%
+# autoplot()
 
-#try randomforest
+# try randomforest
 
 bank_rf_workflow <-
   workflow() %>%
   add_model(rf_mod) %>%
   add_recipe(bank_rec)
 
-##step_novel
+## step_novel
 
 bank_rf_fit <-
   bank_rf_workflow %>%
   fit(data = df_train)
 
 rf_pred <-
-    predict(bank_rf_fit,
-            df_test,
-            type = "prob") %>%
+  predict(bank_rf_fit,
+    df_test,
+    type = "prob"
+  ) %>%
   bind_cols(df_test %>% select(y))
 
 rf_pred %>%
@@ -135,22 +175,23 @@ rf_pred %>%
   autoplot()
 
 
-##decision tree now
+## decision tree now
 bank_tr_workflow <-
   workflow() %>%
   add_model(rpart_mod) %>%
   add_recipe(bank_rec)
 
-##step_novel
+## step_novel
 
 bank_tr_fit <-
   bank_tr_workflow %>%
   fit(data = df_train)
 
 tr_pred <-
-    predict(bank_tr_fit,
-            df_test,
-            type = "prob") %>%
+  predict(bank_tr_fit,
+    df_test,
+    type = "prob"
+  ) %>%
   bind_cols(df_test %>% select(y))
 
 tr_pred %>%
